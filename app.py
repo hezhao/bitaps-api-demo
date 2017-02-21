d8feba25c522a075eec3c8d9d9c1fc98e40e07e6
hezhao <zhao.he@wk.com>
2017-02-21 12:23:33 -0800

    added checkout

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+import coin
 import json
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+coin = Coin()
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/checkout", methods=['POST'])
+def checkout():
+    source = coin.create_source()
+    print('source id: ' + source.id)
+    coin.generate_qrcode(source.bitcoin.uri)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 @app.route("/notifications", methods=['POST'])
 def notifications():
@@ -16,11 +27,20 @@ def notifications():
         source = payload['data']['object']
         source_id = source['id']
         print('source id: ' + source_id)
+        # This method should be called in the callback,
+        # source is retrieved from source.chargeable event payload
+        charge = coin.create_charge(
+            amount=source['amount'],
+            currency=source['currency'],
+            source=source_id,
+            receipt_email=source['owner']['email'],
+            description=source['owner']['email'],
+            metadata=source['metadata'])
+        print(charge)
     elif payload_type == 'source.canceled':
         source = payload['data']['object']
         source_id = source['id']
         print('source id: ' + source_id)
-        print(json.dumps(source))
     elif payload_type == 'charge.succeeded':
         charge = payload['data']['object']
         source = charge['source']
@@ -28,7 +48,7 @@ def notifications():
         source_id = source['id']
         print('source id: ' + source_id)
         print('charge id: ' + charge_id)
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
 if __name__ == "__main__":
