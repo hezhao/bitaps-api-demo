2d0dbd272026d6aa958b54756d0af75f2ab40ded
hezhao <zhao.he@wk.com>
2017-02-21 11:26:09 -0800

    print

--- app.py
+++ app.py
@@ -10,23 +10,22 @@ def hello():
 @app.route("/notifications", methods=['POST'])
 def paid():
     payload = request.json
-    # print(json.dumps(payload))
     payload_type = payload['type']
     print('payload type: ' + payload_type)
     if payload_type == 'source.chargeable':
         source = payload['data']['object']
         source_id = source['id']
         print('source id: ' + source_id)
+    elif payload_type == 'source.canceled':
+        source = payload['data']['object']
+        print(source)
     elif payload_type == 'charge.succeeded':
         charge = payload['data']['object']
         charge_id = charge['id']
         print(charge)
         print('charge id: ' + charge_id)
-    return jsonify(request.json)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-@app.route("/orders/<order_id>/receipt")
-def receipt(order_id):
-    return str(order_id)
 
 if __name__ == "__main__":
     app.run()
